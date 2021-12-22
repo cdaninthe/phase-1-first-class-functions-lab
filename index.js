70f2d6b28a610668367e98087ffc21e3aeb81c49
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2021-12-22 10:57:11 -0500

    commit tree ff6b9f3c47a5783e2a51e099b32c18c47159f4e2

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (drivers){
+    //return drivers.slice((drivers.length - 2), (drivers.length));
+    return drivers.slice (-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (num){
+    return function (fare){
+        return fare*num;
+    };
+}
+
+const fareDoubler = createFareMultiplier (2);
+
+const fareTripler = createFareMultiplier (3);
+
+function selectDifferentDrivers(drivers, moreDrivers) {
+    if (moreDrivers === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    }
+    else if (moreDrivers === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers);
+    }
+}
